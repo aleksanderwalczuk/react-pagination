0ea59c9e213dd4e071fc732f3a116428a4b78a3e
rednaxela700 <ohleg@windowslive.com>
2019-11-23 22:17:08 +0100

    added pagination logic as Pagination.js content

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
-const Pagination = () => {
+const Pagination = ({ postsPerPage, totalPosts }) => {
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pageNumbers.push(i);
+  }
   return (
     <div>
-
+      {pageNumbers.map(pageNumber => (
+        <li
+          key={pageNumber}
+          className='page-item'
+          >
+            <a href ="!#" className="page-link">
+              {pageNumber}
+            </a>
+          </li>
+      ))}
     </div>
-  )
-}
+  );
+};
+export default Pagination
